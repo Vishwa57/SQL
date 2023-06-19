a8e8b4f5ba8b87cf9d47650dfe6d41e3800d4ec3
Vishwa Srinivasan <gvishwa27@gmail.com>
2023-06-19 20:50:23 +0530

    Update LeetCode.sql

--- LeetCode.sql
+++ LeetCode.sql
@@ -139,4 +139,45 @@ select product_name, year, price
 from sales left join product
 on sales.product_id = product.product_id
 
+/*
+Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+Output: 
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+| 54          | 2              |
+| 30          | 1              |
+| 96          | 1              |
++-------------+----------------+
 
+Write a SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+*/
+
+select a.customer_id, count(a.customer_id) as count_no_trans
+from Visits a left join Transactions b
+on a.visit_id = b.visit_id 
+where b.transaction_id is NULL
+group by customer_id

3d8e22be0ef23264f96322c51016c77e975a9b36
Vishwa Srinivasan <gvishwa27@gmail.com>
2023-06-20 08:58:02 +0530

    Update LeetCode.sql

--- LeetCode.sql
+++ LeetCode.sql
@@ -217,3 +217,68 @@ select a.id
 from weather a inner join weather b
 on datediff(a.recorddate, b.recorddate)=1
 where a.temperature > b.temperature
+
+/*
+There is a factory website that has several machines each running the same number of processes. Write an SQL query to find the average time each machine takes to complete a process.
+
+The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+
+The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activity table:
++------------+------------+---------------+-----------+
+| machine_id | process_id | activity_type | timestamp |
++------------+------------+---------------+-----------+
+| 0          | 0          | start         | 0.712     |
+| 0          | 0          | end           | 1.520     |
+| 0          | 1          | start         | 3.140     |
+| 0          | 1          | end           | 4.120     |
+| 1          | 0          | start         | 0.550     |
+| 1          | 0          | end           | 1.550     |
+| 1          | 1          | start         | 0.430     |
+| 1          | 1          | end           | 1.420     |
+| 2          | 0          | start         | 4.100     |
+| 2          | 0          | end           | 4.512     |
+| 2          | 1          | start         | 2.500     |
+| 2          | 1          | end           | 5.000     |
++------------+------------+---------------+-----------+
+Output: 
++------------+-----------------+
+| machine_id | processing_time |
++------------+-----------------+
+| 0          | 0.894           |
+| 1          | 0.995           |
+| 2          | 1.456           |
++------------+-----------------+
+*/
+ --Write your MySQL query statement below
+
+ -- Method 1
+select distinct a.machine_id, 
+round(avg(abs(a.timestamp - b.timestamp)),3) as processing_time
+from Activity a inner join Activity b
+on a.machine_id = b.machine_id 
+and a.process_id = b.process_id
+and a.timestamp < b.timestamp
+group by a.machine_id
+
+ -- Method 2
+SELECT a1.machine_id, ROUND(AVG(ABS(a1.timestamp - a2.timestamp)),3) AS processing_time
+FROM (SELECT *
+FROM Activity
+WHERE activity_type = 'start') a1,
+(SELECT *
+FROM Activity
+WHERE activity_type = 'end') a2
+WHERE a1.machine_id = a2.machine_id
+AND a1.process_id = a2.process_id
+GROUP BY a1.machine_id;
